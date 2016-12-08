8205fcac6f4a154fbfb88e16ba71f8bf33189eb5
Plamen Nikolov <pnikolov@csumb.edu>
2016-12-07 19:47:32 -0800

    more speec attempts

--- Project3.py
+++ Project3.py
@@ -1,9 +1,12 @@
 import speech_recognition as sr
 import pyaudio
 import os
+import sys
 import datetime
 import random
-import pyttsx
+#import pyttsx
+import pyaudio
+from espeak import espeak
 r=sr.Recognizer()
 output=""
 time=datetime.datetime.now()
@@ -11,14 +14,12 @@ jokes={1:"I'd tell you a chemistry joke, but I know I wouldn't get a reaction.",
 2:"What is grey and can't fly? A parking lot.",
 3:"Why don't some couples go to the gym? Because some relationships don't work out.",
 4:"Why did the scientist install a knocker on his door? He wanted to win the No-bell prize.",
-5:"If Apple made a car, would it have Windows?", 
-6:"We dont code anymore. When i code were gonna make code great again. itll be tremendous believe me", 
+5:"If Apple made a car, would it have Windows?",
+6:"We dont code anymore. When i code were gonna make code great again. itll be temendous believe me", 
 7:"Where is the chicken? Idk he crossed the road. ohhhhh", 
 8:"Your life", 
 9:"I've been programming since before steve had a job.",
 10:"Humor is like food. Not everyone gets it - Joseph Stalin."}
-
-
 while output!="exit":
     print
     with sr.Microphone(device_index=2,sample_rate=44100,chunk_size=8192) as source:
@@ -30,29 +31,30 @@ while output!="exit":
         print("You said "+r.recognize_google(audio))
         output=r.recognize_google(audio)
         if "hello" in output:
-		print("Hello! Hope you're doing well today!")
-		print("next")
-		engine = pyttsx.init()
-		engine.say('Please leave me alone')
-		engine.runAndWait()
+            print("Hello! Hope you're doing well today!")
+            print("next")
+            espeak.synth('you suck')
+            #engine = pyttsx.init()
+            #engine.say('Please leave me alone')
+            #engine.runAndWait()
         elif "what time is it" in output:
-        	print ("The time is "+str(time))
+            print ("The time is "+str(time))
         elif "tell a joke" in output:
-		print (jokes.get(random.randint(1,10)))
-		engine = pyttsx.init()
-		engine.say('I hope you enjoyed that')
-		engine.runAndWait()
+            print (jokes.get(random.randint(1,10)))
+            #engine = pyttsx.init()
+            #engine.say('I hope you enjoyed that')
+            #engine.runAndWait()
         elif "seriously" in output:
-		print("Of course! I am a very serious speech recognition;)")
-		engine = pyttsx.init()
-		engine.say('You are seriously failing')
-		engine.runAndWait()
+            print("Of course! I am a very serious speech recognition;)")
+            #engine = pyttsx.init()
+            #engine.say('You are seriously failing')
+            #engine.runAndWait()
+        elif "Google" in output:
+            webbrowser.get('firefox').open_new('http://www.google.com/#q='+output[7:50])
     except LookupError:
-		print("I could not understand, what you said")
+        print("I could not understand, what you said")
     except sr.UnknownValueError:
-		print("I could not understand, what you said")
+        print("I could not understand, what you said")
     except sr.RequestError as e:
-		print("Could not I wanna try smtn request results from Google Speech Recognition service; {0}".format(e))
+        print("Could not I wanna try smtn request results from Google Speech Recognition service; {0}".format(e))
 print("Bye! See you soon!")
-
-
